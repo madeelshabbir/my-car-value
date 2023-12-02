280680790904cb8dfa262fcef0c527a11cbfbdbf
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-12-03 00:41:58 +0500

    Removing Users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -24,13 +24,17 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
 
-    if (!user) {
-      throw new Error('User not found');
-    }
+    if (!user) throw new Error('User not found');
 
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) throw new Error('User not found');
+
+    return this.repo.remove(user);
+  }
 }
