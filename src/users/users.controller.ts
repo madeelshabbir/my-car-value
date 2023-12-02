44012a1ccc144c8e8bd52c6f426cd27513e3def6
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-12-03 01:02:17 +0500

    Removing Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, Param } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,5 +20,10 @@ export class UsersController {
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -13,8 +13,13 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  findOne(id: number) {
-    return this.repo.findOneBy({ id });
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id });
+
+    console.log(user);
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
   }
 
   find() {
@@ -23,18 +28,12 @@ export class UsersService {
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
-
-    if (!user) throw new Error('User not found');
-
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
 
   async remove(id: number) {
     const user = await this.findOne(id);
-
-    if (!user) throw new Error('User not found');
-
     return this.repo.remove(user);
   }
 }
