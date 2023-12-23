080d48da64443964e3e1c0a34d93b0c0198c61f8
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-12-23 22:44:27 +0500

    More mock implementations

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -10,18 +11,40 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeUsersService = {
-      find: () => {},
-      findOne: () => {},
-      update: () => {},
-      remove: () => {},
+      find: () =>
+        Promise.resolve([
+          {
+            id: 1,
+            email: 'asdf@gmail.com',
+            password: 'asdf',
+          } as User,
+        ]),
+      findOne: (id: number) =>
+        Promise.resolve({
+          id,
+          email: 'asdf@gmail.com',
+          password: 'asdf',
+        } as User),
+      // update: () => {},
+      // remove: () => {},
     };
     fakeAuthService = {
-      signup: () => {},
-      signin: () => {},
+      // signup: () => {},
+      // signin: () => {},
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
