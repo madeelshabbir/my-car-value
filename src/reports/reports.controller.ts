10cd6dfdeb93d1bd52871a93a0ccfb0fbcec65ff
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-12-24 19:50:49 +0500

    The ManyToOne and OneToMany Decorators

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
